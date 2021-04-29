612d39902413981989723a55853ec3543a194530
fredylams <82283487+fredylams@users.noreply.github.com>
2021-04-29 18:28:30 -0500

    Create sumaSuperior1LuisAlfredoMartinezSantiago3201.cpp

---
+++ sumaSuperior1LuisAlfredoMartinezSantiago3201.cpp
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+
+float limiteMinimo,limiteMaximo; 
+int rectangulos=10000;  
+float ancho;
+float x,y;
+float area;
+
+printf("\nEste programa calculara el %crea de la funci%cn: y = -(x-7)^2 +19 \n",160,162);
+printf("en los intervalos de 2.68 a 11.32 \n\n");
+
+printf("Ingrese el l%cmite m%cnimo: ",161,161);
+scanf("%f",&limiteMinimo);
+
+printf("Ingrese el l%cmite m%cximo: ",161,160);
+scanf("%f",&limiteMaximo);
+
+system("cls");
+
+printf("\n**Suma inferior**\n\n");
+printf("Datos: L%cmite m%cnimo: %.2f \n ",161,161,limiteMinimo);
+printf("      L%cmite m%cximo: %.2f \n ",161,160,limiteMaximo);
+
+ancho = (limiteMaximo - limiteMinimo)/rectangulos;
+printf("\n Ancho: %.2f\n\n",ancho);
+
+for( x=limiteMinimo ; x<=limiteMaximo ; x=x+ancho ){
+
+    y= (x*x)+2;
+
+    area = area + (ancho*y);
+
+}
+printf("\nEl area es: %.2f \n\n",area);
+
+system("pause");
+return 0;
+}
