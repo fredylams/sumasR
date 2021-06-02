88676ee1acae8fce6153e25f4c956959406d9450
fredylams <82283487+fredylams@users.noreply.github.com>
2021-06-01 19:31:48 -0500

    Create Cálculo_de_integrales_definidasLuisAlfredoMartinezSantiago_3201.m

---
+++ Cálculo_de_integrales_definidasLuisAlfredoMartinezSantiago_3201.m
@@ -0,0 +1,9 @@
+clear all
+clc
+syms x
+f=input('Ingrese la funcion a integrar ')
+a=input('Ingrese el intervalo inferior ')
+b=input('Ingrese el intervalo superior ')
+i=int(f,x,a,b)
+j= i*0.305
+ezplot(f)
